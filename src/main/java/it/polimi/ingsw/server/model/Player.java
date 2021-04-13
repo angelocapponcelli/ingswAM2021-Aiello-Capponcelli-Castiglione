40e005099ee26ed506663f164625f3ee1ff64b3c
tommasoaiello <tommaso.aiello@mail.polimi.it>
2021-04-13 11:26:16 +0200

    I made some new classes and I did part of the already existing class "Player"

--- src/main/java/it/polimi/ingsw/server/model/Player.java
+++ src/main/java/it/polimi/ingsw/server/model/Player.java
@@ -3,4 +3,26 @@ package it.polimi.ingsw.server.model;
 //TODO: all of the class
 
 public class Player {
+    protected String name;
+    protected Integer faithPosition;
+    protected VaticanReportStatus vaticanReportStatus;
+
+    public Player (String name, Integer faithPosition, VaticanReportStatus vaticanReportStatus){
+        this.name = name;
+        this.faithPosition= faithPosition;
+        this.vaticanReportStatus= vaticanReportStatus;
+    }
+    public String getName() { return name; }
+
+    public Integer getFaithPosition() {
+        return faithPosition;
+    }
+//da fare getvaticanreportstatus
+//da fare dovaticanreportupdate
+
+    public void increaseFaithPosition(){
+        this.faithPosition= this.faithPosition+1;
+    }
+
 }
+
